09aef1a0a36f9e30991ebbf189033d0448d38f43
Daniel Cheng <danielchengml@gmail.com>
2017-09-11 22:56:24 -0700

    It will display a completed webpage with bootstrap styling. Also, there are two clickable phrases that displays the original and the encoded sentences respoectively.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
-var sentence = prompt("Please type in a sentence to generate your cipher.");
+$(document).ready(function() {
+	$(".original-sentence").click(function() {
+		alert(sentence);
+	})
 
-//alert (sentence);
+$(".encoded-sentence").click(function() {
+	alert(reverseString(sentence));
+	})
+})
+
+var sentence = prompt("Please type in a sentence to generate your cipher.");
 
 var capsFirstLast = function(input1) {
 	return input1.charAt(0).toUpperCase() + sentence.slice(-1).toUpperCase();
@@ -14,6 +22,10 @@ var outputFirstLast = function(input3) {
 	return input3 + reverseFirstLast(capsFirstLast(input3));
 }
 
+var addFirstLast = function(input4) {
+	return sentence.charAt(Math.floor(sentence.length/2)) + outputFirstLast(input4);
+}
 
-
-alert(outputFirstLast(sentence));
+var reverseString = function(input5) {
+	return addFirstLast(input5).split('').reverse().join('');
+}
